8457b94ab608d7f8ee9e7036a56edd74ae6f2f43
Brennan Novak <brennan@reverseproductions.com>
2011-07-15 19:00:21 -0700

    added api controller

---
+++ config/install.php
@@ -0,0 +1,21 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:			Social Igniter : App Template : Install
+* Author: 		Brennan Novak
+* 		  		contact@social-igniter.com
+*          
+* Created: 		Brennan Novak
+*
+* Project:		http://social-igniter.com/
+* Source: 		http://github.com/socialigniter/app-template
+*
+* Description: 	Install values for App Template for Social Igniter 
+*/
+
+/* Settings */
+$config['app-template']['enabled']				= 'TRUE';
+$config['app-template']['create_permission'] 	= '3';
+$config['app-template']['publish_permission']	= '2';
+$config['app-template']['manage_permission']	= '2';
+$config['app-template']['comments_per_page']	= '5';
+$config['app-template']['comments_allow']		= 'no';
---
+++ controllers/api.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends Oauth_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+	}
+
+    /* Install App */
+	function install_get()
+	{
+		// Load
+		$this->load->library('installer');
+		$this->load->config('install');        
+
+		// Settings & Create Folders
+		$settings = $this->installer->install_settings('app-template', config_item('app_template_settings'));
+	
+		if ($settings == TRUE)
+		{
+            $message = array('status' => 'success', 'message' => 'Yay, the App Template was installed');
+        }
+        else
+        {
+            $message = array('status' => 'error', 'message' => 'Dang App Template could not be uninstalled');
+        }		
+		
+		$this->response($message, 200);
+	} 
+
+}
